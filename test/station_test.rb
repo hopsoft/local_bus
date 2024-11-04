9a3b0d6902abb749ba2f8555a988d50ebe96b4cd
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-11-04 10:31:02 -0700

    Print benchmark for action introspection

--- test/station_test.rb
+++ test/station_test.rb
@@ -96,6 +96,7 @@ class LocalBus
 
       station.publish(@topic, success: true).wait
 
+      ap duration: Time.now - start
       assert Time.now - start < 1.5
       assert_equal 100, received_messages.size
       assert received_messages.all? { _1.payload == {success: true} }
